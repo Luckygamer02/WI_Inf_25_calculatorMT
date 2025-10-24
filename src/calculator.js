b196a97df394e5e7583eac1c92f342e4b873e686
Kristina Popp <kristina2.popp@gmail.com>
2025-10-24 14:48:54 +0200

    Fakultät hinzugefügt

--- src/calculator.js
+++ src/calculator.js
@@ -5,4 +5,13 @@ function div(a, b) {
 if (b === 0) throw new Error('DivideByZeroException');
 return a / b;
 }
-module.exports = { add, sub, mul, div};
+function fac(n) {
+    if (n < 0) throw new Error('Negative numbers not allowed');
+    if (n === 0) return 1;
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+module.exports = { add, sub, mul, div, fac};
